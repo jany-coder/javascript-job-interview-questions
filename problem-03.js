ccc9ed8bb93a643629909ac142d5d2a55c268bcf
jany_coder <jany_ete@yahoo.com>
2023-01-19 15:44:55 +0600

    resize an array

--- problem-03.js
+++ problem-03.js
@@ -1,6 +1,8 @@
 // Problem-03: How to resize an array?
-// Problem-04: How to flatten a multidimensional array?
-// Problem-05: How to use short conditionals?
-// Problem-06: How to replace all occurances of a string?
-// Problem-07: How to log variable with values properly?
-// Problem-08: How to calculate performance of a task/operation?
+
+let animals = ["dog", "monkey", "cat", "fox"];
+
+//we can use array's length property
+animals.length = 3;
+
+console.log(animals); //[ 'dog', 'monkey', 'cat' ]
---
+++ problem-04.js
@@ -0,0 +1,5 @@
+// Problem-04: How to flatten a multidimensional array?
+// Problem-05: How to use short conditionals?
+// Problem-06: How to replace all occurances of a string?
+// Problem-07: How to log variable with values properly?
+// Problem-08: How to calculate performance of a task/operation?
