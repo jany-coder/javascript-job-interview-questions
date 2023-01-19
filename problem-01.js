4743a04a99a3a88a346a8ff85ee700c2e54dcb40
jany_coder <jany_ete@yahoo.com>
2023-01-19 14:42:05 +0600

    first commit

---
+++ problem-01.js
@@ -0,0 +1,23 @@
+// Problem-01: How to remove falsy values from an array?
+// Problem-02: Convart any value to boolean
+// Problem-03: How to resize an array?
+// Problem-04: How to flatten a multidimensional array?
+// Problem-05: How to use short conditionals?
+// Problem-06: How to replace all occurances of a string?
+// Problem-07: How to log variable with values properly?
+// Problem-08: How to calculate performance of a task/operation?
+
+/*
+    Problem-01: How to remove falsy values from an array?
+
+    There are 6 falsy values in javascript:
+
+    false
+    0 (zero)
+    "" (empty string)
+    null
+    undefined
+    NaN (Not a Number)  
+
+*/
+
